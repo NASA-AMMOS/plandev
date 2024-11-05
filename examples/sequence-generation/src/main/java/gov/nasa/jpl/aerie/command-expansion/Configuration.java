d516f1dbab95ac831f546dce7a68f46144ccf3e9
Carter Mak <Carter.Mak@jpl.nasa.gov>
2024-11-05 08:13:08 -0800

    Revert "WIP"
    This reverts commit 226fc2a334b48df7612b9bee32cfc760c0658155.

--- examples/sequence-generation/src/main/java/gov/nasa/jpl/aerie/command-expansion/Configuration.java
+++
@@ -1,4 +0,0 @@
-package gov.nasa.jpl.aerie.command_expansion;
-
-public final class Configuration {
-}
--- examples/sequence-generation/src/main/java/gov/nasa/jpl/aerie/command-expansion/expansion/SequenceStep.java
+++
@@ -1,8 +0,0 @@
-package gov.nasa.jpl.aerie.command_expansion.expansion;
-
-import gov.nasa.jpl.aerie.command_expansion.model.Mission;
-import gov.nasa.jpl.aerie.command_expansion.model.sequencing.FcplSequenceEngine;
-
-public interface SequenceStep {
-    void call(Mission mission, FcplSequenceEngine engine);
-}

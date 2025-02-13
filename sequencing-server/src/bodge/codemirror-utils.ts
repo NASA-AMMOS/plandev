b415f246c1997a74c7b08736909fe9d0da4f9bda
Carter Mak <Carter.Mak@jpl.nasa.gov>
2025-02-13 11:43:38 -0800

    Cleaned up bodge a bit

--- sequencing-server/src/bodge/codemirror-utils.ts
+++ sequencing-server/src/bodge/codemirror-utils.ts
@@ -1,6 +1,6 @@
-import type { SyntaxNode } from '@lezer/common';
+// Only using: removeEscapedQuotes, unquoteUnescape
+
 import type {
-  CommandDictionary,
   FswCommand,
   FswCommandArgument,
   FswCommandArgumentBoolean,
@@ -14,11 +14,7 @@ import type {
   FswCommandArgumentVarString,
   HwCommand,
 } from '@nasa-jpl/aerie-ampcs';
-import type { VariableDeclaration } from '@nasa-jpl/seq-json-schema/types';
-import type { EditorView } from 'codemirror';
-import type { ArgTextDef, NumberArg, StringArg } from '../../types/sequencing';
-import { fswCommandArgDefault } from '../sequence-editor/command-dictionary';
-import type { CommandInfoMapper } from './commandInfoMapper';
+import type { NumberArg, StringArg } from './types/sequencing';
 
 export function isFswCommand(command: FswCommand | HwCommand): command is FswCommand {
   return (command as FswCommand).type === 'fsw_command';
@@ -74,74 +70,6 @@ export function isNumberArg(arg: FswCommandArgument): arg is NumberArg {
 
 export function isStringArg(arg: FswCommandArgument): arg is StringArg {
   return isFswCommandArgumentVarString(arg) || isFswCommandArgumentFixedString(arg);
-}
-
-export function addDefaultArgs(
-  commandDictionary: CommandDictionary,
-  view: EditorView,
-  commandNode: SyntaxNode,
-  argDefs: FswCommandArgument[],
-  commandInfoMapper: CommandInfoMapper,
-) {
-  const insertPosition = commandInfoMapper.getArgumentAppendPosition(commandNode);
-  if (insertPosition !== undefined) {
-    const str = commandInfoMapper.formatArgumentArray(
-      argDefs.map(argDef => fswCommandArgDefault(argDef, commandDictionary.enumMap)),
-      commandNode,
-    );
-    const transaction = view.state.update({
-      changes: { from: insertPosition, insert: str },
-    });
-    view.dispatch(transaction);
-  }
-}
-
-export function getMissingArgDefs(argInfoArray: ArgTextDef[]): FswCommandArgument[] {
-  return argInfoArray
-    .filter((argInfo): argInfo is { argDef: FswCommandArgument } => !argInfo.node && !!argInfo.argDef)
-    .map(argInfo => argInfo.argDef);
-}
-
-export function getDefaultVariableArgs(parameters: VariableDeclaration[]): string[] {
-  return parameters.map(parameter => {
-    switch (parameter.type) {
-      case 'STRING':
-        return `"${parameter.name}"`;
-      case 'FLOAT':
-        return parameter.allowable_ranges && parameter.allowable_ranges.length > 0
-          ? parameter.allowable_ranges[0].min
-          : 0;
-      case 'INT':
-      case 'UINT':
-        return parameter.allowable_ranges && parameter.allowable_ranges.length > 0
-          ? parameter.allowable_ranges[0].min
-          : 0;
-      case 'ENUM':
-        return parameter.allowable_values && parameter.allowable_values.length > 0
-          ? `"${parameter.allowable_values[0]}"`
-          : parameter.enum_name
-            ? `${parameter.enum_name}`
-            : 'UNKNOWN';
-      default:
-        throw Error(`unknown argument type ${parameter.type}`);
-    }
-  }) as string[];
-}
-
-export function addDefaultVariableArgs(
-  parameters: VariableDeclaration[],
-  view: EditorView,
-  commandNode: SyntaxNode,
-  commandInfoMapper: CommandInfoMapper,
-) {
-  const insertPosition = commandInfoMapper.getArgumentAppendPosition(commandNode);
-  if (insertPosition !== undefined) {
-    const str = commandInfoMapper.formatArgumentArray(getDefaultVariableArgs(parameters), commandNode);
-    const transaction = view.state.update({
-      changes: { from: insertPosition, insert: str },
-    });
-    view.dispatch(transaction);
-  }
 }
 
 export function isQuoted(s: string): boolean {

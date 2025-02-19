03a64e02fd46e976d61ab9b901b428ee772a7336
JosephVolosin <joseph.p.volosin@nasa.gov>
2025-02-19 17:59:12 -0500

    Refactor SequenceDefintiion => SequenceFilter

--- deployment/postgres-init-db/sql/tables/sequencing/sequence_filter.sql
+++ deployment/postgres-init-db/sql/tables/sequencing/sequence_filter.sql
@@ -1,10 +1,10 @@
-create table sequencing.sequence_definition (
+create table sequencing.sequence_filter (
   id integer generated always as identity,
   filter jsonb not null default '{}'::jsonb,
   model_id integer not null,
   name text,
 
-  constraint sequence_definition_primary_key
+  constraint sequence_filter_primary_key
   primary key (id),
 
   foreign key (model_id)

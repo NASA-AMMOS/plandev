8d2040a22c90897c1218901eee569f78c6932667
pranav-super <pranav.subramanian@colorado.edu>
2025-02-27 13:29:20 -0800

    add trigger that verifies language consistency for given model/parcel combo

--- deployment/postgres-init-db/sql/tables/sequencing/sequence_template.sql
+++ deployment/postgres-init-db/sql/tables/sequencing/sequence_template.sql
@@ -26,4 +26,28 @@ create table sequencing.sequence_template (
 
   constraint only_one_template_per_model_activity_type
       unique (model_id, activity_type)
-)
+);
+
+-- Introduce a trigger that verifies that, for a given (model_id, parcel_id) tuple, the language is consistent.
+--    TODO: Discuss alternative implementations. This is fine, but should language be in parcel? That works but sort of breaks when considering that
+--          parcel is in use in the legacy implementation of sequencing. But what about using workspaces? For now, leaving as is, but worth discussing further.
+create or replace function sequencing.check_language_sameness()
+	returns trigger
+	language plpgsql as $$
+begin
+	-- If there are no entries for this model,parcel tuple, proceed.
+	if (select count(language) as existing_entries from sequencing.sequence_template where model_id=1 and parcel_id=1 limit 1) <= 0 then
+		return new;
+	end if;
+	-- If there are entries for this model,parcel tuple, ensure the language is the same.
+    if (not new.language = (select language from sequencing.sequence_template where model_id=new.model_id and parcel_id=new.parcel_id limit 1)) then
+		raise foreign_key_violation
+		using message='Languages must be the same for a given (model_id, parcel_id) tuple.';
+	end if;
+	return new;
+end;
+$$;
+
+create or replace trigger ensure_language_match
+before insert or update on sequencing.sequence_template
+	for each row execute function sequencing.check_language_sameness();

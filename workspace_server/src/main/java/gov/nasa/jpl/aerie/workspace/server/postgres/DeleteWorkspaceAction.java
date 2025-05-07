2e8d648ca01b1d5e6ec56a981b094ee79a214f55
Theresa Kamerman <theresa.kamerman@jpl.nasa.gov>
2025-05-07 12:56:27 -0700

    DeleteWorkspaceAction fix class privacy

--- workspace_server/src/main/java/gov/nasa/jpl/aerie/workspace/server/postgres/DeleteWorkspaceAction.java
+++ workspace_server/src/main/java/gov/nasa/jpl/aerie/workspace/server/postgres/DeleteWorkspaceAction.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class DeleteWorkspaceAction implements AutoCloseable {
+/*package-local*/ final class DeleteWorkspaceAction implements AutoCloseable {
   private static final @Language("SQL") String sql = """
     delete from sequencing.workspace
     where id = ?

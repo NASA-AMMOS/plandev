71416c22ef01864565c2e00d41c206e046bc347d
Brad Clement <bclement@jpl.nasa.gov>
2025-06-09 09:17:00 -0700

    use DefaultEditablePlanDriver

--- procedural/examples/banana-procedures/src/main/java/gov/nasa/ammos/aerie/procedural/examples/bananaprocedures/standalone/Demo.java
+++ procedural/examples/banana-procedures/src/main/java/gov/nasa/ammos/aerie/procedural/examples/bananaprocedures/standalone/Demo.java
@@ -1,16 +1,17 @@
 package gov.nasa.ammos.aerie.procedural.examples.bananaprocedures.standalone;
 
+import gov.nasa.ammos.aerie.procedural.scheduling.utils.DefaultEditablePlanDriver;
+import gov.nasa.ammos.aerie.procedural.timeline.collections.profiles.Real;
+import gov.nasa.ammos.aerie.procedural.timeline.plan.SimulationResults;
+import gov.nasa.ammos.aerie.procedural.utils.TypeUtilsEditablePlanAdapter;
+import gov.nasa.ammos.aerie.procedural.utils.TypeUtilsPlanAdapter;
 import gov.nasa.jpl.aerie.banananation.Configuration;
-import gov.nasa.jpl.aerie.banananation.Mission;
 import gov.nasa.jpl.aerie.banananation.generated.GeneratedModelType;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 
-import gov.nasa.jpl.aerie.merlin.driver.SimulationResults;
 import gov.nasa.jpl.aerie.orchestration.simulation.SimulationUtility;
 import gov.nasa.jpl.aerie.types.Plan;
 import gov.nasa.jpl.aerie.types.Timestamp;
@@ -22,20 +23,25 @@ public class Demo {
     final var simStartTime = Instant.EPOCH;
     final var missionModel = SimulationUtility.instantiateMissionModel(new GeneratedModelType(), simStartTime, c);
 
+
     Plan plan = new Plan("plan0",
                          new Timestamp(simStartTime),
                          new Timestamp(simStartTime.plus(1, ChronoUnit.HOURS)),
                          Map.of(),
                          Map.of());
     try (final var simulationUtility = new SimulationUtility()) {
-      var results =
-          simulationUtility.simulate(missionModel, plan).get();
-      //var latest = new MerlinToProcedureSimulationResultsAdapter(results, plan);
-      System.out.println(results.realProfiles);
-    } catch (InterruptedException e) {
-      throw new RuntimeException(e);
-    } catch (ExecutionException e) {
-      throw new RuntimeException(e);
+
+      var planDriver = new DefaultEditablePlanDriver(
+          new TypeUtilsEditablePlanAdapter(
+              new TypeUtilsPlanAdapter(plan),
+              simulationUtility,
+              missionModel
+          )
+      );
+
+      final SimulationResults results = planDriver.simulate();
+      final Real fruit = results.resource("/fruit", Real.deserializer());
+      System.out.println(fruit.collect());
     }
   }
 }

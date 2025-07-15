8978284f40a45e535c9fc7d56fd49c3aab561daf
Brad Clement <bclement@jpl.nasa.gov>
2025-07-15 10:06:23 -0700

    add a second simulation

--- procedural/examples/banana-procedures/src/main/java/gov/nasa/ammos/aerie/procedural/examples/bananaprocedures/standalone/Demo.java
+++ procedural/examples/banana-procedures/src/main/java/gov/nasa/ammos/aerie/procedural/examples/bananaprocedures/standalone/Demo.java
@@ -2,6 +2,7 @@ package gov.nasa.ammos.aerie.procedural.examples.bananaprocedures.standalone;
 
 import gov.nasa.ammos.aerie.procedural.scheduling.utils.DefaultEditablePlanDriver;
 import gov.nasa.ammos.aerie.procedural.timeline.collections.profiles.Real;
+import gov.nasa.ammos.aerie.procedural.timeline.payloads.activities.DirectiveStart;
 import gov.nasa.ammos.aerie.procedural.timeline.plan.SimulationResults;
 import gov.nasa.ammos.aerie.procedural.utils.TypeUtilsEditablePlanAdapter;
 import gov.nasa.ammos.aerie.procedural.utils.TypeUtilsPlanAdapter;
@@ -12,6 +13,8 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Map;
 
+import gov.nasa.jpl.aerie.merlin.protocol.types.Duration;
+import gov.nasa.jpl.aerie.merlin.protocol.types.SerializedValue;
 import gov.nasa.jpl.aerie.orchestration.simulation.SimulationUtility;
 import gov.nasa.jpl.aerie.types.Plan;
 import gov.nasa.jpl.aerie.types.Timestamp;
@@ -39,9 +42,16 @@ public class Demo {
           )
       );
 
-      final SimulationResults results = planDriver.simulate();
-      final Real fruit = results.resource("/fruit", Real.deserializer());
-      System.out.println(fruit.collect());
+      SimulationResults results = planDriver.simulate();
+      Real fruit = results.resource("/fruit", Real.deserializer());
+      System.out.println("/fruit: " + fruit.collect());
+
+      planDriver.create("BiteBanana", new DirectiveStart.Absolute(Duration.MINUTE),
+                        Map.of("biteSize", SerializedValue.of(1.5)));
+      results = planDriver.simulate();
+      fruit = results.resource("/fruit", Real.deserializer());
+      System.out.println("/fruit: " + fruit.collect());
+      System.out.println("activity instances: " + results.instances().collect());
     }
   }
 }

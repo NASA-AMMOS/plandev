78e9c481bf82bc93806dc6e8a08861246ef303f9
joelco <joel.courtney@jpl.nasa.gov>
2025-07-29 15:49:36 -0700

    Add doc comments

--- contrib/src/main/java/gov/nasa/jpl/aerie/contrib/models/NamedResource.java
+++ contrib/src/main/java/gov/nasa/jpl/aerie/contrib/models/NamedResource.java
@@ -2,6 +2,9 @@ package gov.nasa.jpl.aerie.contrib.models;
 
 import gov.nasa.jpl.aerie.merlin.framework.resources.NameableResource;
 
+/**
+ * Provides a field for setting the string name of the resource.
+ */
 public abstract class NamedResource<D> implements NameableResource<D> {
   private String name = "ERROR: Name was not set during model construction";
 
--- procedural/timeline/src/main/kotlin/gov/nasa/ammos/aerie/procedural/timeline/util/WithModel.kt
+++ procedural/timeline/src/main/kotlin/gov/nasa/ammos/aerie/procedural/timeline/util/WithModel.kt
@@ -1,5 +1,18 @@
 package gov.nasa.ammos.aerie.procedural.timeline.util
 
+/**
+ * A mixin for accessing a singleton instance of the mission model object.
+ *
+ * Just make your goal or constraint `implements WithModel<MyModel>`. You can
+ * then access the mission model with `this.model()`. This interface makes no
+ * guarantees about the simulation configuration the model was instantiated with.
+ * It will most likely be the default config, but that is not a formal
+ * requirement. The only guarantee is that it will be a valid instance of the class.
+ *
+ * The type `M` provided to `WithModel<M>` is not checked at compile time.
+ * Giving the wrong type will result in a runtime class cast exception when
+ * `model()` is called.
+ */
 interface WithModel<M> {
   @Suppress("unchecked_cast")
   fun model(): M {

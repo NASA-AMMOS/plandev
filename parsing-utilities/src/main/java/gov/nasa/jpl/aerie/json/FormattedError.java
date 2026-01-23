e27cd66d4c0271a2a631226595f63858e211c898
Theresa Kamerman <tkamerma@ndc.nasa.gov>
2026-01-23 12:37:56 -0500

    Move FormattedError to parsing-utilities

--- parsing-utilities/src/main/java/gov/nasa/jpl/aerie/json/FormattedError.java
+++ parsing-utilities/src/main/java/gov/nasa/jpl/aerie/json/FormattedError.java
@@ -1,16 +1,9 @@
-package gov.nasa.jpl.aerie.workspace.server;
+package gov.nasa.jpl.aerie.json;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import gov.nasa.jpl.aerie.permissions.exceptions.Forbidden;
-import gov.nasa.jpl.aerie.permissions.exceptions.PermissionsServiceException;
-import gov.nasa.jpl.aerie.workspace.server.exceptions.FileLockedException;
-import gov.nasa.jpl.aerie.workspace.server.exceptions.MalformedRequest;
-import gov.nasa.jpl.aerie.workspace.server.exceptions.NoSuchFileException;
-import gov.nasa.jpl.aerie.workspace.server.exceptions.WorkspaceFileOpException;
-import gov.nasa.jpl.aerie.workspace.server.postgres.NoSuchWorkspaceException;
 import io.javalin.http.UnauthorizedResponse;
 import io.javalin.validation.ValidationException;
 
@@ -27,16 +20,26 @@ import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 /**
- * Class for formatting exceptions thrown during in Workspaces into JSON objects
- * that meet the Aerie HTTP endpoint error message format
+ * Class for formatting exceptions thrown into JSON objects that meet the Aerie HTTP endpoint error message format
  * Relevant ticket going over said format: https://github.com/NASA-AMMOS/plandev/issues/1732
  */
 @JsonSerialize(using = FormattedError.FormattedErrorSerializer.class)
-public final class FormattedError {
+public class FormattedError {
+  public enum AerieService {
+    MERLIN_SERVER("aerie_merlin"),
+    SCHEDULER_SERVER("aerie_scheduler"),
+    WORKSPACE_SERVER("aerie_workspace"),
+    PERMISSIONS_SERVICE("aerie_permissions");
+
+    private final String serviceName;
+    AerieService(String serviceName) { this.serviceName = serviceName; }
+    String serviceName() {return serviceName;}
+  }
+
   private final String type;
   private final String message;
   private final String timestamp = ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_INSTANT);
-  private final static String service = "aerie_workspace";
+  private final AerieService service;
   private Optional<String> cause = Optional.empty();
   private Optional<String> trace = Optional.empty();
   private Optional<JsonObject> data = Optional.empty();
@@ -45,17 +48,19 @@ public final class FormattedError {
    * For use in the event of an endpoint failing without throwing an exception.
    * i.e. Workspace's delete file endpoint failing because java.nio.File#deleteFile returned "false"
    */
-  public FormattedError(String message) {
+  public FormattedError(AerieService service, String message) {
     this.type = "INTERNAL_ERROR";
     this.message = message;
+    this.service = service;
   }
 
   /**
    * For use in the event of an endpoint failing without throwing an exception, but where there's a more detailed cause.
    */
-  public FormattedError(String message, String cause) {
+  public FormattedError(AerieService service, String message, String cause) {
     this.type = "INTERNAL_ERROR";
     this.message = message;
+    this.service = service;
     this.cause = Optional.ofNullable(cause);
   }
 
@@ -63,9 +68,10 @@ public final class FormattedError {
    * For use in the event of an endpoint failing without throwing an exception,
    *  but "INTERNAL_ERROR" does not make sense as the error type (i.e. the request is malformed)
    */
-  public FormattedError(String type, String message, Optional<String> cause) {
+  public FormattedError(AerieService service, String type, String message, Optional<String> cause) {
     this.type = type;
     this.message = message;
+    this.service = service;
     this.cause = cause;
   }
 
@@ -74,9 +80,10 @@ public final class FormattedError {
    * @param type the category of exception. Should be in SCREAMING_SNAKE_CASE
    * @param ex the exception to be formatted.
    */
-  public FormattedError(String type, Exception ex) {
+  public FormattedError(AerieService service, String type, Exception ex) {
     this.type = type;
     message = ex.getMessage() == null ? "No exception message provided." : ex.getMessage();
+    this.service = service;
     trace = Optional.of(generateTrace(ex));
   }
 
@@ -88,66 +95,36 @@ public final class FormattedError {
    *  Should be human-readable and between 1-2 sentences.
    * @param ex the exception to be formatted.
    */
-  public FormattedError(String type, String message, Exception ex) {
+  public FormattedError(AerieService service, String type, String message, Exception ex) {
     this.type = type;
     this.message = message;
+    this.service = service;
     cause = Optional.ofNullable(ex.getMessage());
     trace = Optional.of(generateTrace(ex));
   }
 
   // region Constructors for specific exceptions
   //  This helps `type` be consistent every time the exception is thrown.
-  // NoSuchWorkspace
-  public FormattedError(NoSuchWorkspaceException nse) {
-    this("NO_SUCH_WORKSPACE", nse);
-  }
-  public FormattedError(NoSuchWorkspaceException nse, String message) {
-    this("NO_SUCH_WORKSPACE", message, nse);
-  }
-
-  public FormattedError(gov.nasa.jpl.aerie.permissions.exceptions.NoSuchWorkspaceException nse, String message) {
-    this("NO_SUCH_WORKSPACE", message, nse);
-  }
-
-  // NoSuchFile
-  public FormattedError(NoSuchFileException nsf) {
-    this("NO_SUCH_FILE", nsf.getMessage(), nsf);
-  }
-  public FormattedError(NoSuchFileException nsf, String message) {
-    this("NO_SUCH_FILE", message, nsf);
-  }
 
   // IOException
-  public FormattedError(IOException nse) {
-    this("IO_EXCEPTION", nse);
+  public FormattedError(AerieService service, IOException nse) {
+    this(service, "IO_EXCEPTION", nse);
   }
-  public FormattedError(IOException nse, String message) {
-    this("IO_EXCEPTION", message, nse);
+  public FormattedError(AerieService service, IOException nse, String message) {
+    this(service, message, nse);
   }
 
   // SQLException
-  public FormattedError(SQLException se) {
-    this("SQL_EXCEPTION", se);
+  public FormattedError(AerieService service, SQLException se) {
+    this(service, "SQL_EXCEPTION", se);
   }
-  public FormattedError(SQLException se, String message) {
-    this("SQL_EXCEPTION", message, se);
-  }
-
-  // WorkspaceFileOpException
-  public FormattedError(WorkspaceFileOpException wfe) {
-    this("FILE_OPERATION_EXCEPTION", wfe);
-  }
-  public FormattedError(WorkspaceFileOpException wfe, String message) {
-    this("FILE_OPERATION_EXCEPTION", message, wfe);
-  }
-
-  // Forbidden
-  public FormattedError(Forbidden ue) {
-    this("FORBIDDEN", ue);
+  public FormattedError(AerieService service, SQLException se, String message) {
+    this(service, "SQL_EXCEPTION", message, se);
   }
 
   // Unauthorized
-  public FormattedError(UnauthorizedResponse ue) {
+  public FormattedError(AerieService service, UnauthorizedResponse ue) {
+    this.service = service;
     this.type = "UNAUTHORIZED";
     this.message = ue.getMessage() != null ? ue.getMessage() : "Unauthorized";
     // Include additional details, if present
@@ -158,58 +135,41 @@ public final class FormattedError {
     }
   }
 
-  // PermissionsServiceException
-  public FormattedError(PermissionsServiceException pse, String message) {
-    this("PERMISSIONS_SERVICE_EXCEPTION", message, pse);
-  }
-
   // NumberFormatException
-  public FormattedError(NumberFormatException nfe) {
-    this("NUMBER_PARSING_EXCEPTION", nfe);
+  public FormattedError(AerieService service, NumberFormatException nfe) {
+    this(service, "NUMBER_PARSING_EXCEPTION", nfe);
   }
 
   // IllegalArgumentException
-  public FormattedError(IllegalArgumentException iae) {
-    this("ILLEGAL_ARGUMENT", iae);
+  public FormattedError(AerieService service, IllegalArgumentException iae) {
+    this(service, "ILLEGAL_ARGUMENT", iae);
   }
 
   // JSONException
-  public FormattedError(JsonException je, String message){
-    this("JSON_PARSING_EXCEPTION", message, je);
+  public FormattedError(AerieService service, JsonException je){
+    this(service, "JSON_PARSING_EXCEPTION", je);
+  }
+
+  public FormattedError(AerieService service, JsonException je, String message){
+    this(service, "JSON_PARSING_EXCEPTION", message, je);
   }
 
   // ValidationException
-  public FormattedError(ValidationException ve) {
+  public FormattedError(final AerieService service, ValidationException ve) {
+    this.service = service;
     this.type = "ENDPOINT_VALIDATION_EXCEPTION";
     this.message = ve.getMessage() != null ? ve.getMessage() : "Invalid request";
     trace = Optional.of(generateTrace(ve));
   }
 
   // Null Pointer Exception
-  public FormattedError(NullPointerException ne, String message) {
-    this("NULL_POINTER_EXCEPTION", message, ne);
+  public FormattedError(AerieService service, NullPointerException ne, String message) {
+    this(service, "NULL_POINTER_EXCEPTION", message, ne);
   }
 
   // Security Exception
-  public FormattedError(SecurityException se) {
-    this("SECURITY_EXCEPTION", se.getMessage(), se);
-  }
-
-  // Malformed Request
-  public FormattedError(MalformedRequest mr) {
-    this.type = "MALFORMED_REQUEST";
-    this.message = mr.getMessage();
-    this.cause = mr.getDetails();
-    this.trace = Optional.of(generateTrace(mr));
-  }
-
-  // Locked File
-  public FormattedError(FileLockedException fle) {
-    this("FILE_LOCKED", fle);
-  }
-
-  public FormattedError(FileLockedException fle, String message) {
-    this("FILE_LOCKED", message, fle);
+  public FormattedError(AerieService service, SecurityException se) {
+    this(service, "SECURITY_EXCEPTION", se.getMessage(), se);
   }
   //endregion
 
@@ -224,6 +184,9 @@ public final class FormattedError {
     return sw.toString();
   }
 
+  public String getType() { return type; }
+  public String getMessage() { return message; }
+
   /**
    * Export this object to a JsonObject.
    */
@@ -233,7 +196,7 @@ public final class FormattedError {
                             .add("type", type)
                             .add("message", message)
                             .add("timestamp", timestamp)
-                            .add("service", service); // Not mandatory on spec, but always known
+                            .add("service", service.serviceName()); // Not mandatory on spec, but always known
 
     // Include optional fields, if present
     cause.ifPresent((c) -> builder.add("cause", c));
@@ -253,7 +216,7 @@ public final class FormattedError {
    * This avoids needing to call `toJson` every time the FormattedError class is used as an endpoint return.
    * The class implements Jackson's JsonSerializer in specific because Javalin uses Jackson as its default JSON Mapper.
    */
-  static final class FormattedErrorSerializer extends JsonSerializer<FormattedError> {
+  public static final class FormattedErrorSerializer extends JsonSerializer<FormattedError> {
     @Override
     public void serialize(
         final FormattedError formattedError,

62603513a842a74f39bc65e6ba323cd7f457a80a
psubram3 <pranav.subramanian@nasa.gov>
2026-05-22 16:13:30 -0700

    add created_at to source reference

--- deployment/postgres-init-db/sql/tables/merlin/external_events/external_source.sql
+++ deployment/postgres-init-db/sql/tables/merlin/external_events/external_source.sql
@@ -11,7 +11,7 @@ create table merlin.external_source (
     attributes jsonb not null default '{}',
 
     constraint external_source_pkey
-      primary key (key, derivation_group_name),
+      primary key (key, derivation_group_name, created_at),
     -- a given dg cannot have two sources with the same valid_at!
     CONSTRAINT dg_unique_valid_at UNIQUE (derivation_group_name, valid_at),
     constraint external_source_references_external_source_type_name

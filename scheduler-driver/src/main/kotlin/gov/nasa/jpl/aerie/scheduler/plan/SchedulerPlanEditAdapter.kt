e5448cb6ba427a79a61b11da60f6fde0c67f7fdf
psubram3 <pranav.subramanian@nasa.gov>
2026-05-26 15:38:29 -0700

    handle sourcelist when converting directive to SchedulingActivity

--- scheduler-driver/src/main/kotlin/gov/nasa/jpl/aerie/scheduler/plan/SchedulerPlanEditAdapter.kt
+++ scheduler-driver/src/main/kotlin/gov/nasa/jpl/aerie/scheduler/plan/SchedulerPlanEditAdapter.kt
@@ -1,20 +1,20 @@
 package gov.nasa.jpl.aerie.scheduler.plan
 
-import gov.nasa.jpl.aerie.merlin.driver.MissionModel
-import gov.nasa.jpl.aerie.merlin.protocol.types.Duration
 import gov.nasa.ammos.aerie.procedural.scheduling.simulation.SimulateOptions
 import gov.nasa.ammos.aerie.procedural.scheduling.utils.DefaultEditablePlanDriver
 import gov.nasa.ammos.aerie.procedural.scheduling.utils.PerishableSimulationResults
-import gov.nasa.jpl.aerie.scheduler.simulation.SimulationFacade
 import gov.nasa.ammos.aerie.procedural.timeline.payloads.activities.AnyDirective
 import gov.nasa.ammos.aerie.procedural.timeline.payloads.activities.Directive
 import gov.nasa.ammos.aerie.procedural.timeline.payloads.activities.DirectiveStart
+import gov.nasa.ammos.aerie.procedural.timeline.plan.Plan
+import gov.nasa.jpl.aerie.merlin.driver.MissionModel
+import gov.nasa.jpl.aerie.merlin.protocol.types.Duration
 import gov.nasa.jpl.aerie.merlin.protocol.types.DurationType
 import gov.nasa.jpl.aerie.scheduler.DirectiveIdGenerator
 import gov.nasa.jpl.aerie.scheduler.model.*
-import gov.nasa.jpl.aerie.types.ActivityDirectiveId
+import gov.nasa.jpl.aerie.scheduler.simulation.SimulationFacade
+import gov.nasa.jpl.aerie.types.*
 import kotlin.jvm.optionals.getOrNull
-import gov.nasa.ammos.aerie.procedural.timeline.plan.Plan;
 
 /*
  * An implementation of [EditablePlan] that stores the plan in memory for use in the internal scheduler.
@@ -52,6 +52,43 @@ data class SchedulerPlanEditAdapter(
   }
 
   companion object {
+    fun convertActivitySources(activitySources: List<gov.nasa.ammos.aerie.procedural.timeline.payloads.reference.ActivitySource<*>>): List<ActivitySource<out Any>> {
+      if (activitySources.size > 0) {
+        return activitySources.map {
+          when (it) {
+            is gov.nasa.ammos.aerie.procedural.timeline.payloads.reference.ResourceActivitySource ->
+              ResourceActivitySource(it.v)
+
+            is gov.nasa.ammos.aerie.procedural.timeline.payloads.reference.DirectiveActivitySource ->
+              DirectiveActivitySource(
+                ActivityDirective(
+                  it.v.startTime,
+                  it.v.name,
+                  it.v.inner.arguments,
+                  it.v.id,
+                  true, // TODO
+                  it.v.activitySources?.let { it1 -> convertActivitySources(it1) },
+                )
+              )
+
+            is gov.nasa.ammos.aerie.procedural.timeline.payloads.reference.ExternalEventActivitySource ->
+              ExternalEventActivitySource(
+                ExternalEvent(
+                  it.v.key,
+                  it.v.type,
+                  it.v.source.derivationGroup,
+                  it.v.source.key,
+                  it.v.source.createdAt
+                )
+              )
+
+            else -> ResourceActivitySource("get rid of this, not possible")
+          }
+        }
+      }
+      return emptyList()
+    }
+
     @JvmStatic fun Directive<AnyDirective>.toSchedulingActivity(lookupActivityType: (String) -> ActivityType) = SchedulingActivity(
         id,
         lookupActivityType(type),
@@ -81,7 +118,8 @@ data class SchedulerPlanEditAdapter(
         is DirectiveStart.Absolute -> true
         is DirectiveStart.Anchor -> s.anchorPoint == DirectiveStart.Anchor.AnchorPoint.Start
       },
-      name
+      name,
+      activitySources?.let { convertActivitySources(it) }
     )
   }
 }

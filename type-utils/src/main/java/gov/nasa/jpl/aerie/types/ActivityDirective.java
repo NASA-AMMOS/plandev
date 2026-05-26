122b31580e5048d7ed11ab6c208a2ab57a516964
psubram3 <pranav.subramanian@nasa.gov>
2026-05-26 15:37:23 -0700

    add source list to ActivityDirective type

--- type-utils/src/main/java/gov/nasa/jpl/aerie/types/ActivityDirective.java
+++ type-utils/src/main/java/gov/nasa/jpl/aerie/types/ActivityDirective.java
@@ -3,24 +3,46 @@ package gov.nasa.jpl.aerie.types;
 import gov.nasa.jpl.aerie.merlin.protocol.types.Duration;
 import gov.nasa.jpl.aerie.merlin.protocol.types.SerializedValue;
 
+import java.util.List;
 import java.util.Map;
 
 public record ActivityDirective(
     Duration startOffset,
     SerializedActivity serializedActivity,
     ActivityDirectiveId anchorId, // anchorId can be null
-    boolean anchoredToStart
-//    List<ActivitySource> sourceList -> TODO
+    boolean anchoredToStart,
+    List<ActivitySource<?>> sourceList
 ) {
   public ActivityDirective(
       final Duration startOffset,
       final String type,
       final Map<String, SerializedValue> arguments,
       final ActivityDirectiveId anchorId,
-      final boolean anchoredToStart) {
-    this(startOffset,
-         new SerializedActivity(type, (arguments != null) ? Map.copyOf(arguments) : null),
-         anchorId,
-         anchoredToStart);
+      final boolean anchoredToStart,
+      final List<ActivitySource<?>> sourceList)
+  {
+    this(
+        startOffset,
+        new SerializedActivity(type, (arguments != null) ? Map.copyOf(arguments) : null),
+        anchorId,
+        anchoredToStart,
+        sourceList
+    );
+  }
+
+  public ActivityDirective(
+      final Duration startOffset,
+      final String type,
+      final Map<String, SerializedValue> arguments,
+      final ActivityDirectiveId anchorId,
+      final boolean anchoredToStart)
+  {
+    this(
+        startOffset,
+        new SerializedActivity(type, (arguments != null) ? Map.copyOf(arguments) : null),
+        anchorId,
+        anchoredToStart,
+        List.of()
+    );
   }
 }

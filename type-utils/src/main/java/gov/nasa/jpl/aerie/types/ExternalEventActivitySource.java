a1dfed5e4aea5a1a29c8498e8e4f4563ebb01338
psubram3 <pranav.subramanian@nasa.gov>
2026-05-29 19:33:53 -0700

    minor typo in ExternalEventActivitySource

--- type-utils/src/main/java/gov/nasa/jpl/aerie/types/ExternalEventActivitySource.java
+++ type-utils/src/main/java/gov/nasa/jpl/aerie/types/ExternalEventActivitySource.java
@@ -3,8 +3,8 @@ package gov.nasa.jpl.aerie.types;
 public final class ExternalEventActivitySource implements ActivitySource<ExternalEvent> { // TODO, make External Event somehow
   ExternalEvent value;
 
-  public ExternalEventActivitySource(ExternalEvent resourceName) {
-    value = resourceName;
+  public ExternalEventActivitySource(ExternalEvent externalEvent) {
+    value = externalEvent;
   }
 
   @Override

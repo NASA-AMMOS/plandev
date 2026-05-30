e4bed0f34d95ae68d45a999c41296e3085e588d1
psubram3 <pranav.subramanian@nasa.gov>
2026-05-29 17:18:36 -0700

    add scheduling_sources view

---
+++ deployment/postgres-init-db/sql/views/merlin/scheduling_sources.sql
@@ -0,0 +1,13 @@
+create view merlin.scheduling_sources as
+(
+  select
+    a.scheduled_directive_id,
+    array_agg(concat('a: ', text(a.referenced_directive_id)))
+      || array_agg(concat('r: ', r.referenced_resource_name)) as sources
+  from
+    merlin.directive_source_is_activity as a
+  join
+    merlin.directive_source_is_resource_type as r
+  on a.scheduled_directive_id = r.scheduled_directive_id
+  group by a.scheduled_directive_id, a.scheduled_plan_id, r.referenced_resource_model_id
+);

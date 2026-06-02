2af3af26a65334e1c8bc847132191ba99388f820
psubram3 <pranav.subramanian@nasa.gov>
2026-06-02 07:35:53 -0700

    modify the migration

--- deployment/hasura/migrations/Aerie/35_add_scheduling_sources_view/down.sql
+++ deployment/hasura/migrations/Aerie/35_add_scheduling_sources_view/down.sql
@@ -1,3 +1,5 @@
+drop trigger merlin.trg_invalidate_b_on_date_change;
+drop function merlin.update_directive_source_is_activity_invalid;
 drop view merlin.scheduling_sources;
 
 call migrations.mark_migration_rolled_back(35);

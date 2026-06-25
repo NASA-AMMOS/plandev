bba0d8ba04a46c1fc81d23e147369e617c61e418
Theresa Kamerman <tkamerma@ndc.nasa.gov>
2026-06-25 16:41:58 -0400

    Use Optional instead of null for eTags

--- workspace-server/src/main/java/gov/nasa/jpl/aerie/workspace/server/types/HandlerResult.java
+++ workspace-server/src/main/java/gov/nasa/jpl/aerie/workspace/server/types/HandlerResult.java
@@ -4,15 +4,16 @@ import gov.nasa.jpl.aerie.workspace.server.FormattedError;
 
 import javax.json.Json;
 import javax.json.JsonValue;
+import java.util.Optional;
 
 public sealed interface HandlerResult {
   int status();
   JsonValue jsonResponse();
 
-  record Success(int status, String response, String etag) implements HandlerResult {
-    /** For successes with no ETag (directories, moves, copies, deletes). */
+  record Success(int status, String response, Optional<String> etag) implements HandlerResult {
+    /** For successes with no ETag (directory creation, moves, copies, deletes). */
     public Success(int status, String response) {
-      this(status, response, null);
+      this(status, response, Optional.empty());
     }
     @Override
     public JsonValue jsonResponse() {

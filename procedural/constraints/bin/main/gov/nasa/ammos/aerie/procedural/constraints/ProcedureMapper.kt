0f2494bf03b9205067750ae224894a326ed67ee0
Remy Rabideau <66102692+remy-rabideau@users.noreply.github.com>
2026-07-27 15:55:15 -0700

    Remove tracked bin/ build artifacts (should be gitignored)

--- procedural/constraints/bin/main/gov/nasa/ammos/aerie/procedural/constraints/ProcedureMapper.kt
+++
@@ -1,9 +0,0 @@
-package gov.nasa.ammos.aerie.procedural.constraints
-
-import gov.nasa.jpl.aerie.merlin.protocol.model.InputType
-import gov.nasa.jpl.aerie.merlin.protocol.types.ValueSchema
-
-interface ProcedureMapper<T: Constraint> {
-  fun valueSchema(): ValueSchema
-  fun getInputType(): InputType<T>
-}

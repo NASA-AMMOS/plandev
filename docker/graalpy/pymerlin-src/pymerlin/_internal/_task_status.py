664c780dba3e3e55d87bacd30b3e3c57dc88cfef
remy-rabideau <remy.c.rabideau@gmail.com>
2026-08-05 16:53:20 -0700

    Revert "Dev files and local pymerlin"
    This reverts commit dd433a75a4e1df3612b97035373dd143c0fffc0c.

--- docker/graalpy/pymerlin-src/pymerlin/_internal/_task_status.py
+++
@@ -1,6 +0,0 @@
-from collections import namedtuple
-
-Completed = namedtuple("Completed", "value")
-Delayed = namedtuple("Delayed", "duration")
-Calling = namedtuple("Calling", "child")
-Awaiting = namedtuple("Awaiting", "condition")

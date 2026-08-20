639cd23b3017ab9c55f763f2ab32644e8fa90398
Theresa Kamerman <tkamerma@ndc.nasa.gov>
2026-08-20 16:55:55 -0400

    Rename DB credentials from Aerie to Plandev
    - support backwards compatibility in Docker Compose

--- deployment/postgres-init-db/init-plandev.sh
+++ deployment/postgres-init-db/init-plandev.sh
@@ -2,9 +2,12 @@
 
 set -e
 
+export ADMIN_DB_USER="${PLANDEV_USERNAME}"
+export ADMIN_DB_PASS="${PLANDEV_PASSWORD}"
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname postgres <<-EOSQL
   \echo 'Initializing aerie user...'
-  CREATE USER "$AERIE_USERNAME" WITH PASSWORD '$AERIE_PASSWORD';
+  CREATE USER "$ADMIN_DB_USER" WITH PASSWORD '$ADMIN_DB_PASS';
   \echo 'Done!'
 
   \echo 'Initializing gateway user...'
@@ -24,22 +27,22 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname postgres <<-EOSQL
   \echo 'Done!'
 
   \echo 'Initializing "$PLANDEV_DB" database...'
-  CREATE DATABASE "$PLANDEV_DB" OWNER "$AERIE_USERNAME";
+  CREATE DATABASE "$PLANDEV_DB" OWNER "$ADMIN_DB_USER";
   \connect "$PLANDEV_DB"
-  ALTER SCHEMA public OWNER TO "$AERIE_USERNAME";
+  ALTER SCHEMA public OWNER TO "$ADMIN_DB_USER";
   \connect postgres
   \echo 'Done!'
 
   \echo 'Initializing "$PLANDEV_METADATA_DB" database...'
   CREATE DATABASE "$PLANDEV_METADATA_DB";
-  GRANT ALL PRIVILEGES ON DATABASE "$PLANDEV_METADATA_DB" TO "$AERIE_USERNAME";
+  GRANT ALL PRIVILEGES ON DATABASE "$PLANDEV_METADATA_DB" TO "$ADMIN_DB_USER";
   \echo 'Done!'
 EOSQL
 
-export PGPASSWORD="$AERIE_PASSWORD"
+export PGPASSWORD="$ADMIN_DB_PASS"
 
-psql -v ON_ERROR_STOP=1 --username "$AERIE_USERNAME" --dbname "$PLANDEV_DB" <<-EOSQL
-  \set aerie_user $AERIE_USERNAME
+psql -v ON_ERROR_STOP=1 --username "$ADMIN_DB_USER" --dbname "$PLANDEV_DB" <<-EOSQL
+  \set aerie_user $ADMIN_DB_USER
   \set gateway_user $GATEWAY_DB_USER
   \set merlin_user $MERLIN_DB_USER
   \set scheduler_user $SCHEDULER_DB_USER

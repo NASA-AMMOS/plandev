63a29f853194a5c1eecdd5f4636cc62c20aee671
Theresa Kamerman <tkamerma@ndc.nasa.gov>
2026-08-25 18:21:11 -0400

    Fix sequencing not loading plandev db name

--- sequencing-server/src/db.ts
+++ sequencing-server/src/db.ts
@@ -6,6 +6,7 @@ import { getEnv } from './env.js';
 const { Pool: DbPool } = pg;
 
 const {
+  PLANDEV_DB: database,
   PLANDEV_DB_HOST: host,
   PLANDEV_DB_PORT: port,
   SEQUENCING_DB_USER: user,
@@ -24,7 +25,7 @@ export class DbExpansion {
   static init() {
     try {
       const config: PoolConfig = {
-        database: 'aerie',
+        database,
         host,
         password,
         port: parseInt(port, 10),

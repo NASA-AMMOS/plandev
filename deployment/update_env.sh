7e17c5e30f8d826058b67720b17e6f2beeed0e33
Theresa Kamerman <tkamerma@ndc.nasa.gov>
2026-08-28 16:27:58 -0400

    Add script to update env and Gateway container permissions

---
+++ deployment/update_env.sh
@@ -0,0 +1,9 @@
+# Fix Gateway Container Permissions
+docker compose exec -u root plandev_gateway chown -R node:node /app/files
+
+# Add new envvars to the .env
+GATEWAY_CONTAINER_ID=$(docker ps | grep gateway | awk {'print $1'})
+VOLUME_NAME=$(docker inspect -f '{{ .Mounts }}' $GATEWAY_CONTAINER_ID | grep aerie_file_store | awk {'print $2'})
+echo PLANDEV_FILE_STORE_NAME="$VOLUME_NAME" | tee -a .env
+echo PLANDEV_DATABASE_NAME=aerie | tee -a .env
+echo PLANDEV_METADATA_DATABASE_NAME=aerie_hasura | tee -a .env

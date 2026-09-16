27f258e4cdc4b5a717149a744f25171b0b2ac215
AaronPlave <aaronplave@gmail.com>
2026-09-16 14:53:20 -0700

    Add mission_model constraint to check jar presence when model is executable

--- deployment/postgres-init-db/sql/tables/merlin/mission_model.sql
+++ deployment/postgres-init-db/sql/tables/merlin/mission_model.sql
@@ -28,6 +28,8 @@ create table merlin.mission_model (
     foreign key (owner) references permissions.users
     on update cascade
     on delete set null,
+  constraint mission_model_executable_jar_exists
+    check (is_executable = false or jar_id is not null),
   foreign key (default_view_id)
     references ui.view
     on delete set null

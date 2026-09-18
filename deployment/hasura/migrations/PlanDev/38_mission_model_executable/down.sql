16d71fae090c98e0aa05ef793a577ce2c8b6839b
Theresa Kamerman <tkamerma@ndc.nasa.gov>
2026-09-18 08:09:25 -0400

    Add readonly check function
    - fix ordering in down.sql

--- deployment/hasura/migrations/PlanDev/38_mission_model_executable/down.sql
+++ deployment/hasura/migrations/PlanDev/38_mission_model_executable/down.sql
@@ -1,31 +1,35 @@
-alter table merlin.mission_model
-  rename constraint mission_model_references_file to mission_model_references_jar;
+-- Drop readonly check function
+drop procedure merlin.plan_readonly_exception(integer);
 
-alter table merlin.mission_model
-  drop column is_executable;
-
-alter table merlin.mission_model
-  rename column definition_file_id to jar_id;
-
-comment on column merlin.mission_model.jar_id is e''
-    'An uploaded JAR file defining the mission model.';
-
-create or replace function merlin.increment_revision_mission_model_jar_update()
-returns trigger
-security definer
-language plpgsql as $$begin
-  update merlin.mission_model
-  set revision = revision + 1
-  where jar_id = new.id
-    or jar_id = old.id;
-
-  return new;
-end$$;
-
+-- Drop added column on Plan
 alter table merlin.plan
   drop column is_read_only;
 
 comment on column merlin.plan.is_locked is e''
   'A boolean representing whether this plan can be deleted and if changes can happen to the activities of this plan.';
+
+-- Update functions that refer to "definition_file_id"
+create or replace function merlin.increment_revision_mission_model_jar_update()
+  returns trigger
+  security definer
+  language plpgsql as $$begin
+  update merlin.mission_model
+  set revision = revision + 1
+  where jar_id = new.id
+     or jar_id = old.id;
+
+  return new;
+end$$;
+
+-- Drop schema updates to Mission Model
+alter table merlin.mission_model
+  rename constraint mission_model_references_file to mission_model_references_jar;
+alter table merlin.mission_model
+  drop column is_executable;
+alter table merlin.mission_model
+  rename column definition_file_id to jar_id;
+
+comment on column merlin.mission_model.jar_id is e''
+    'An uploaded JAR file defining the mission model.';
 
 call migrations.mark_migration_rolled_back('38');
--- deployment/hasura/migrations/PlanDev/38_mission_model_executable/up.sql
+++ deployment/hasura/migrations/PlanDev/38_mission_model_executable/up.sql
@@ -1,9 +1,8 @@
+-- Update schema information on Mission Model
 alter table merlin.mission_model
   rename column jar_id to definition_file_id;
-
 alter table merlin.mission_model
   add column is_executable boolean not null default true;
-
 alter table merlin.mission_model
   rename constraint mission_model_references_jar to mission_model_references_file;
 
@@ -12,6 +11,7 @@ comment on column merlin.mission_model.definition_file_id is e''
 comment on column merlin.mission_model.is_executable is e''
   'A flag that is set to true if the model is executable, otherwise false.';
 
+-- Update functions that refer to old column name "jar_id"
 create or replace function merlin.increment_revision_mission_model_jar_update()
 returns trigger
 security definer
@@ -24,6 +24,7 @@ language plpgsql as $$begin
   return new;
 end$$;
 
+-- Update schema information on Plan
 alter table merlin.plan
   add column is_read_only boolean not null default false;
 
@@ -33,5 +34,18 @@ comment on column merlin.plan.is_locked is e''
   'Managed by the merge workflow independently of the is_read_only setting.';
 comment on column merlin.plan.is_read_only is e''
   'When true, prohibits changes to the plan''s activity directives and time bounds.';
+
+-- Add readonly check function
+create procedure merlin.plan_readonly_exception(plan_id integer)
+  language plpgsql as $$
+begin
+  if(select is_read_only from merlin.plan p where p.id = plan_id limit 1) then
+    raise exception 'Plan % is marked as Read Only and cannot be edited.', plan_id;
+  end if;
+end
+$$;
+
+comment on procedure merlin.plan_readonly_exception(plan_id integer) is e''
+  'Checks whether the specified plan is marked as "read only", throwing an exception if it is.';
 
 call migrations.mark_migration_applied('38');
